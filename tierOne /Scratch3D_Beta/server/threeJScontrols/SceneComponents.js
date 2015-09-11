bebb838d5382a5d2df5d0842ffba79eedcc1e39a
goodwinj14 <goodwinj14@yahoo.com>
2015-09-11 17:46:24 -0400

    Blocks UX change

--- tierOne /Scratch3D_Beta/server/threeJScontrols/SceneComponents.js
+++ tierOne /Scratch3D_Beta/server/threeJScontrols/SceneComponents.js
@@ -12,11 +12,7 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 	//Adds a new cube to our scene based off of the user supplied params
 	if(shape=="Cube"){
 	var cube = null;
-	if(Physics=="On"){
-		cube = new Physijs.BoxMesh(new THREE.CubeGeometry(length, width ,height), Physijs.createMaterial(new THREE.MeshNormalMaterial(),.4,.8),2);
-	}else{
-	cube = new THREE.Mesh(new THREE.CubeGeometry(length, width ,height), new THREE.MeshNormalMaterial());
-	}
+    cube = new THREE.Mesh(new THREE.CubeGeometry(length, width ,height), new THREE.MeshNormalMaterial());
 	cube.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
 		if(cube!=null){
@@ -31,11 +27,7 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 	//Adds a new Shpere to our scene based off of the supplied params
 	if(shape=="Sphere"){
 	var sphere = null;	
-	if(Physics=="On"){
-		sphere = new Physijs.SphereMesh(new THREE.SphereGeometry(length, 50,50), Physijs.createMaterial(new THREE.MeshNormalMaterial(),.4,.8),2);
-	}else{
 	sphere = new THREE.Mesh(new THREE.SphereGeometry(length, 50,50), new THREE.MeshNormalMaterial());
-	}
 	sphere.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
 		if(sphere!=null){
@@ -64,11 +56,7 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 	//Adds a new Cylinder to our scene based off of the supplied params
 	if(shape=="Cylinder"){
 	var cylinder = null; 
-	if(Physics=="On"){
-		cylinder = new Physijs.CylinderMesh( new THREE.CylinderGeometry( length, width, height, 50 ), Physijs.createMaterial(new THREE.MeshNormalMaterial(),.4,.8),2);
-	}else{
-		cylinder = new THREE.Mesh( new THREE.CylinderGeometry( length, width, height, 50 ), new THREE.MeshNormalMaterial() );
-	}
+	cylinder = new THREE.Mesh( new THREE.CylinderGeometry( length, width, height, 50 ), new THREE.MeshNormalMaterial() );
 	cylinder.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
 		if(cylinder!=null){
@@ -82,11 +70,7 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 //Adds a new Dodecahedron to our scene based off of the supplied params
 	if(shape=="Dodecahedron"){
 	var dodecahedron = null;
-	if(Physics=="On"){
-		dodecahedron = new Physijs.ConvexMesh( new THREE.DodecahedronGeometry(length, 0), Physijs.createMaterial(new THREE.MeshNormalMaterial(),.4,.8),2);
-	}else{
-		dodecahedron = new THREE.Mesh( new THREE.DodecahedronGeometry(length, 0), new THREE.MeshNormalMaterial());
-	}
+	dodecahedron = new THREE.Mesh( new THREE.DodecahedronGeometry(length, 0), new THREE.MeshNormalMaterial());
     
 	dodecahedron.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
@@ -101,12 +85,8 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 	//Adds a new Icosahedron to our scene based off of the supplied params
 	if(shape=="Icosahedron"){
 	var icosahedron = null;
-	if(Physics=="On"){
-		icosahedron = new Physijs.ConvexMesh( new THREE.IcosahedronGeometry(length, 0), Physijs.createMaterial(new THREE.MeshNormalMaterial(),.4,.8),2);
-	}else{
-		icosahedron = new THREE.Mesh( new THREE.IcosahedronGeometry(length, 0), new THREE.MeshNormalMaterial());
-		}
-		icosahedron.position.set(LocationX,LocationY,LocationZ);
+	icosahedron = new THREE.Mesh( new THREE.IcosahedronGeometry(length, 0), new THREE.MeshNormalMaterial());
+	icosahedron.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
 		if(icosahedron!=null){
 			Scene.add(icosahedron);
@@ -120,12 +100,8 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 //Adds a new Plane to our scene based off of the supplied params
 	if(shape=="Plane"){
 	var plane = null;
-	if(Physics=="On"){
-		plane = new Physijs.ConvexMesh( new THREE.PlaneGeometry( length, width, 32 ), Physijs.createMaterial(new THREE.MeshNormalMaterial(),.4,.8),2);
-	}else{
-	 plane = new THREE.Mesh( new THREE.PlaneGeometry( length, width, 32 ), new THREE.MeshNormalMaterial());
-	 }
-	 plane.position.set(LocationX,LocationY,LocationZ);
+	plane = new THREE.Mesh( new THREE.PlaneGeometry( length, width, 32 ), new THREE.MeshNormalMaterial());
+	plane.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
 		if(plane!=null){
 			Scene.add(plane);
@@ -138,11 +114,8 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 //Adds a new Ring to our scene based off of the supplied params
 	if(shape=="Ring"){
 		var ring = null;
-		if(Physics=="On"){
-			ring = new Physijs.ConvexMesh( new THREE.RingGeometry( parseFloat(length), parseFloat(width)+1, 32 ), Physijs.createMaterial(new THREE.MeshNormalMaterial(),.4,.8),2);
-	}else{
-			ring = new THREE.Mesh( new THREE.RingGeometry( parseFloat(length), parseFloat(width)+1, 32 ), new THREE.MeshNormalMaterial());
-		}
+		ring = new THREE.Mesh( new THREE.RingGeometry( parseFloat(length), parseFloat(width)+1, 32 ), new THREE.MeshNormalMaterial());
+
 			ring.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
 		if(ring!=null){
@@ -156,11 +129,7 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 //Adds a new Torus to our scene based off of the supplied params
 	if(shape=="Torus"){
 		var torus;
-		if(Physics=="On"){
-			torus = new Physijs.ConvexMesh( new THREE.TorusGeometry( parseFloat(width), parseFloat(length), 32, 100 ), Physijs.createMaterial(new THREE.MeshNormalMaterial(),.4,.8),2);
-	}else{
- 			torus = new THREE.Mesh( new THREE.TorusGeometry( parseFloat(width), parseFloat(length), 32, 100 ), new THREE.MeshNormalMaterial() );
-	}
+ 		torus = new THREE.Mesh( new THREE.TorusGeometry( parseFloat(width), parseFloat(length), 32, 100 ), new THREE.MeshNormalMaterial() );
 		torus.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
 		if(torus!=null){
