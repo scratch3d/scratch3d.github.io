355f61ddfd3b34b6e231479ef6d05740d611f2e1
goodwinj14 <goodwinj14@yahoo.com>
2015-09-18 18:57:43 -0400

    Blocks UX change

--- tierTwo/Scratch3D_Beta/extension/ScratchThreeJS.js
+++ tierTwo/Scratch3D_Beta/extension/ScratchThreeJS.js
@@ -722,7 +722,7 @@
     var descriptor = {
         blocks: [
       // Block type, block name, function name, param1 default value, param2 default value
-      ['w', 'New 3D World %m.Scenes Gravity: %m.Toggle Width: %n Height: %n %m.Camera', 'initWorld', "Grass", 10, 10 ,ext],
+      ['w', 'New 3D World %m.Scenes Width: %n Height: %n', 'initWorld', "Grass", 10, 10 ,ext],
       //['', 'Set Camera Controls  Up: %m.Keys Down: %m.Keys Left: %m.Keys Right: %m.Keys ', 'camControlsMove', 'w', 's','a','d'],
       ['', 'Add Camera Controls %m.CameraControls Move Speed: %n Look Speed: %n ', 'camControls','First Person', '200', '50'],
 			//The camera orbit block to allow users to orbit the camera around a given point
