b265ad443490d9ef7ce05f51ec4a16ce891e6261
goodwinj14 <goodwinj14@yahoo.com>
2015-09-17 20:21:32 -0400

    Blocks UX change

--- tierOne /Scratch3D_Beta/server/threeJScontrols/CompleteCameraControls.js
+++ tierOne /Scratch3D_Beta/server/threeJScontrols/CompleteCameraControls.js
@@ -765,6 +765,12 @@ THREE.CompleteCameraControls = function ( object, domElement ) {
 
 			_this.phi = THREE.Math.mapLinear( _this.phi, 0, Math.PI, _this.verticalMin, _this.verticalMax );
 		}
+
+		targetPosition.x = position.x + 100 * Math.sin( this.phi ) * Math.cos( this.theta );
+		targetPosition.y = position.y + 100 * Math.cos( this.phi );
+		targetPosition.z = position.z + 100 * Math.sin( this.phi ) * Math.sin( this.theta );
+
+		this.object.lookAt( targetPosition );
 	}
 
 	};
