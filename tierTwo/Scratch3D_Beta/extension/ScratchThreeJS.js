b8956de648ba1af30418ac8527f082179209bd58
goodwinj14 <goodwinj14@yahoo.com>
2015-09-22 22:59:48 -0400

    Blocks UX change

--- tierTwo/Scratch3D_Beta/extension/ScratchThreeJS.js
+++ tierTwo/Scratch3D_Beta/extension/ScratchThreeJS.js
@@ -372,6 +372,7 @@ window.addEventListener("message", receiveMessage, false);
     if((charecters.indexOf(objectIdOne)>=0||shapes.indexOf(objectIdOne)>=0)&&(charecters.indexOf(ObjectIdTwo)>=0||shapes.indexOf(ObjectIdTwo)>=0)){
       if(raycasters[objectIdOne]!=null){
         console.log("--ADDOBJECTTOCASTER11111--",raycasters[objectIdOne]);
+        console.log("--ADDOBJECTTOCASTER11111--",ObjectIdTwo);
         if(raycasters[objectIdOne].indexOf(ObjectIdTwo)>=0){
           if(collisions.data[objectIdOne].indexOf(ObjectIdTwo)>=0){
            return true;
