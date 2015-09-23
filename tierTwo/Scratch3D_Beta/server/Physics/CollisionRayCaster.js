f9f7536cdc4e67d1b539d876723621f7e3c0f8ad
goodwinj14 <goodwinj14@yahoo.com>
2015-09-23 01:25:24 -0400

    Blocks UX change

--- tierTwo/Scratch3D_Beta/server/Physics/CollisionRayCaster.js
+++ tierTwo/Scratch3D_Beta/server/Physics/CollisionRayCaster.js
@@ -101,15 +101,10 @@ CollisionDetection.RayCaster = function(caster, casterID, touch, touchID){
 
 	this.CollisionCheck = function(){
 		this.isTouching = [];
-		
+		console.log("Rays Postion: Else was called ", this.mesh.geometry.vertices[1].x);
 		for (var i = this.mesh.geometry.vertices.length - 1; i >= 0; i--) {
 			for(var g = this.rays.length - 1; g >= 0; g--){
 			this.caster.set(this.mesh.geometry.vertices[i], this.rays[g]);
-			if(this.mesh.geometry.vertices[1].x<=.5){
-				//if(this.mesh.geometry.vertices.x<40){
-				console.log("Rays Postion: Else was called ", this.mesh.geometry.vertices);
-				//}
-			}
 			var touching = this.caster.intersectObjects(this.conntactObjects);
 
 			//console.log("Ray castre return values",touching);
