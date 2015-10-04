4b8493d77679482d748b2a1fbb1d356af349046a
goodwinj14 <goodwinj14@yahoo.com>
2015-10-04 00:03:12 -0400

    Blocks UX change

--- tierOne /Scratch3D_Beta/extension/scratch3d.js
+++ tierOne /Scratch3D_Beta/extension/scratch3d.js
@@ -4,8 +4,7 @@
 var sceneWindow = null;
 var $3dmodal = null;
 var loadingDocument = false;
-var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body { margin: 0; } canvas { width: 100%; height: 100% } </style> </head> <body> <script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/r72/three.js'></script> <script> var scene = new THREE.Scene(); var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 ); var renderer = new THREE.WebGLRenderer(); renderer.setSize( window.innerWidth, window.innerHeight ); document.body.appendChild( renderer.domElement ); function addShape(){var geometry = new THREE.BoxGeometry( 1, 1, 1 ); var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); var cube = new THREE.Mesh( geometry, material ); scene.add( cube ); } camera.position.z = 5; var render = function () {requestAnimationFrame( render ); renderer.render(scene, camera); }; addShape(); render(); </script> </body> </html>";
-
+var htmlCode = "<html><head><title>Scratch3d</title><style>body { margin: 0; }canvas { width: 100%; height: 100% }</style></head><body><div id='container' style='width:1425px; height:720px;'></div><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/three.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/CameraControls.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/SceneComponents.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/TrackballControls.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/SceneStyles.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/OBJLoader.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/threex.grassground.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/BlendCharacter.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/CharecterControles.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/tween.min.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/FirstPersonControls.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/lookAtControls.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/fpvObjectControles.js'></script><script src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/threeJScontrols/threex.planets-master/threex.planets.js'></script><script type='text/javascript' src='http://scratch3d.github.io/tierTwo/Scratch3D_Beta/server/Physics/CollisionRayCaster.js'></script><!-- load the font files --><script src='fonts/gentilis_bold.typeface.js'></script><script src='fonts/gentilis_regular.typeface.js'></script><script src='fonts/optimer_bold.typeface.js'></script><script src='fonts/optimer_regular.typeface.js'></script><script src='fonts/helvetiker_bold.typeface.js'></script><script src='fonts/helvetiker_regular.typeface.js'></script><script src='fonts/droid/droid_sans_regular.typeface.js'></script><script src='fonts/droid/droid_sans_bold.typeface.js'></script><script src='fonts/droid/droid_serif_regular.typeface.js'></script><script src='fonts/droid/droid_serif_bold.typeface.js'></script><script>var scene = new THREE.Scene();var cube = null;var camera = new THREE.PerspectiveCamera( 75, document.getElementById('container').offsetWidth/document.getElementById('container').offsetHeight, 0.1, 1000 );var renderer = new THREE.WebGLRenderer();renderer.setSize(document.getElementById('container').offsetWidth,document.getElementById('container').offsetHeight);document.getElementById('container').appendChild( renderer.domElement );function init_window(sceneStyle){console.log('Init Window');init_Scene(sceneStyle);init_Render();init_Camera(6,8,5.6);if(RENDERER==null || SCENE==null || CAMERA==null){return false;}else {firstRender(SCENE, CAMERA); animate(); console.log('RUNN CALLED'); return true;}}//INITIALIZES THE RENDERER OBJECTfunction init_Render(){console.log('init_Render', window.devicePixelRatio );RENDERER = new THREE.WebGLRenderer({ alpha: alpha });RENDERER.setSize(window.innerWidth, window.innerHeight);document.body.appendChild(RENDERER.domElement);}//INITIALIZES THE CAMERA OBJECT//@params Y postion, Z postion, X rotation function init_Camera(posY, posZ, rotaX){console.log('init_Camera');CAMERA = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 1000);CAMERA.position.x = 1; CAMERA.position.y = 1;CAMERA.position.z = 1;CAMERA.rotation.x = 0.0;CAMERA.rotation.y = 0.0;CAMERA.rotation.z = 0.0;}function init_Scene(sceneStyle){SCENE = new THREE.Scene;//Turns the gravity of the scene on to a defualt of -50 along the y axis/*if(sceneStyle[1]=='On'){console.log('gravity: ', sceneStyle[1]);SCENE.setGravity(new THREE.Vector3( 0, -50, 0 ));}*///Creates a basic grid sceneif(sceneStyle[0]=='Grid'){var light= new THREE.AmbientLight( 0x020202 );SCENE.add( light );SCENESTYLES.gridSize = parseInt(sceneStyle[1])*parseInt(sceneStyle[2]);var plane = SCENESTYLES.GRID();SCENE.add(plane);}else if(sceneStyle[0]=='Space'){var light= new THREE.AmbientLight( 0x020202 );SCENE.add( light );var light= new THREE.AmbientLight( 0x020202 );SCENE.add( light );// add a light in frontvar light= new THREE.DirectionalLight('white', 1);light.angle = (Math.PI/5);light.position.set(0.5, 0.5, 2);SCENE.add( light );// add a light behindvar light= new THREE.DirectionalLight('white', 1);light.angle = (Math.PI/5);light.position.set(-0.5, 2.5, -2);SCENE.add( light );var starFeild = THREEx.Planets.createStarfield(parseInt(sceneStyle[1]));SCENE.add(starFeild);}//////////////////////////////Creates a Flat grass scene///////////////////////////else if(sceneStyle[0]=='Grass'){console.log(sceneStyle);alpha = true;//Straight from https://github.com/jeromeetienne/threex.grassground//Thanks jeromeetienne for all the amazing game extentions!!!///////////////////////////////////////////////////////////////////////////////////ADD Light//////////////////////////////var light= new THREE.AmbientLight( 0x020202 );SCENE.add( light );// add a light in frontvar light= new THREE.DirectionalLight('white', 1);light.position.set(0.5, 0.5, 2);SCENE.add( light );// add a light behindvar light= new THREE.DirectionalLight('white', 1);light.position.set(-0.5, 2.5, -2);SCENE.add( light );/////////////////////////////////////////////////////////////////////////////////////Adds Sky//////////////////////////////////////////////////////////////////////////////////////////// directly from @mrdoob in http://mrdoob.com/lab/javascript/webgl/clouds/var canvas= document.createElement( 'canvas' );var context= canvas.getContext( '2d' );canvas.width= 32;canvas.height= window.innerHeight;var gradient= context.createLinearGradient( 0, 0, 0, canvas.height );gradient.addColorStop(0 , '#1e4877');gradient.addColorStop(0.5, '#4584b4');context.fillStyle= gradient;context.fillRect(0, 0, canvas.width, canvas.height);var url= canvas.toDataURL('image/png');document.body.style.background= 'url(' + url + ')';/////////////////////////////////////////////////////////////////////////////////////Adds Ground///////////////////////////////////////////////////////////////////////////////////////var groundMesh = new THREEx.GrassGround({width : 10, // the width of the mesh, default to 1 height : 10, // the height of the mesh, default to 1 repeatX : sceneStyle[1], // the number of time the texture is repeated in X, default to 1 repeatY : sceneStyle[2], // the number of time the texture is repeated in Y, default to 1});//Scales Mesh resulting in a smother grass look gets the average between width and height (((parseInt(sceneStyle[1])+(parseInt(sceneStyle[1]))/2)groundMesh.scale.multiplyScalar((parseInt(sceneStyle[1])+parseInt(sceneStyle[2]))/2);SCENE.add(groundMesh);}}//-----------------------------------------------------------------------------function onWindowResize() { CAMERA.aspect = window.innerWidth / window.innerHeight; CAMERA.updateProjectionMatrix(); RENDERER.setSize( window.innerWidth, window.innerHeight ); }//RENDERS THE THE RENDER OBJECT FOR THE FIRST TIME ONCE THE WINDOW IS CREATEDfunction firstRender(scene, camera){RENDERER.render(scene, camera);} function render(){var delta = 0.75*clock.getDelta();CollisionDetection.update();if(TrackBallControls){RENDERER.render(SCENE, CAMERA);if(controls!=null){controls.update(delta);}}else{if(controls!=null){controls.update(delta);}RENDERER.render(SCENE, CAMERA);}//THREE.AnimationHandler.update( delta );} //Gloabal variables for hadeling camera updatesvar INV_MAX_FPS = 1/60;var frameDelta = 0;function animate(){//THREE.AnimationHandler.update( delta );//TWEEN.update();//SCENE.simulate(); // run physicsif(objControls!=null){objControls.update(clock.getDelta());}requestAnimationFrame(animate);render();}</script></body></html>";
 /*****************Html Three Js Code ***********************/
 /***********************************************************/
 /***********************************************************/
@@ -23,8 +22,8 @@ var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body 
             Camera: ['Perspective'],
             CameraRotation: ['Left', 'Right', 'Up', 'Down', 'Roll Left', 'Roll Right'],
             CameraOrbit: ['Orbit Left', 'Orbit Right', 'Orbit Up', 'Orbit Down'],
-            CameraControls: ["First Person", "Mouse/Trackball"],
-            Sides: ["Back", "Front"],
+            CameraControls: ['First Person', 'Mouse/Trackball'],
+            Sides: ['Back', 'Front'],
             Move: ['Left', 'Right', 'Up', 'Down','Forward','Back'],
             Shapes: ['Cube', 'Sphere', 'Circle','Cylinder', 'Dodecahedron', 'Icosahedron', 'Plane', 'Ring', 'Torus'],
             Planets: ['Earth', 'Sun','Moon', 'Mercury','Venus','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto! #savepluto'],   
@@ -58,6 +57,7 @@ var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body 
     ext.initWorld = function(sceneType, width, height){
         sceneWindow =  window.open();
         sceneWindow.document.write(htmlCode);
+        sceneWindow.document.init_window(sceneType);
     };
 
 
@@ -66,33 +66,33 @@ var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body 
          /*var fileSelector = document.createElement('button');
         fileSelector.setAttribute('id', 'file');
     document.body.appendChild(fileSelector);
-    var evt = document.createEvent("MouseEvents");
-      evt.initEvent("click", true, false);
+    var evt = document.createEvent('MouseEvents');
+      evt.initEvent('click', true, false);
       fileSelector.dispatchEvent(evt);
       console.log(document.getElementById('file'));*/
 
 var inputElement = null;
 function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrapper, data) {
-    elementType = elementType || "div";
-    appendTo = appendTo || "body";
+    elementType = elementType || 'div';
+    appendTo = appendTo || 'body';
     data = data || {};
 
     var $element = $(document.getElementById(elementId));
     if (!$element.length) {
-        var templateContent = "";
-        if (typeof(templateId) != "string") {
+        var templateContent = '';
+        if (typeof(templateId) != 'string') {
             for (var id in templateId) {
                 templateContent += $(document.getElementById(templateId[id])).html();
 
             }
-            console.log("if: ", templateContent);
+            console.log('if: ', templateContent);
         } else {
             templateContent += $(document.getElementById(templateId)).html();
-                templateContent = "<dialog class='extension-warning with-icon'><section><h2>Warning</h2><p>The extensions on this site are experimental</p></section><section><p>The Scratch Team is <strong>not</strong> responsible for the extensions and projects on this site. Please use caution when using these extensions. <a href='#faq'>Learn More</a></p><input type='file' id='upfile'/><output type='file' id='subfile'/><button data-action='show' data-target='home'>Back to ScratchX home</button><button >Uplaod File</button></section></dialog>"
+                templateContent = '<dialog class='extension-warning with-icon'><section><h2>Warning</h2><p>The extensions on this site are experimental</p></section><section><p>The Scratch Team is <strong>not</strong> responsible for the extensions and projects on this site. Please use caution when using these extensions. <a href='#faq'>Learn More</a></p><input type='file' id='upfile'/><output type='file' id='subfile'/><button data-action='show' data-target='home'>Back to ScratchX home</button><button >Uplaod File</button></section></dialog>'
         }
         $template = _.template(templateContent);
-        $element = $("<"+elementType+"></"+elementType+">")
-            .attr("id", elementId)
+        $element = $('<'+elementType+'></'+elementType+'>')
+            .attr('id', elementId)
             .html($template(data));
         if (wrapper) $element.wrapInner(wrapper);
         $element.appendTo(appendTo)
@@ -104,86 +104,86 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
   function sModal(templateId, data) {
     /*
      * Copies the HTML referenced by data-template into a new element,
-     * with id="modal-[template value]" and creates an overlay on the
+     * with id='modal-[template value]' and creates an overlay on the
      * page, which when clicked will close the popup.
      */
 
     var zIndex = 100;
-    var modalId = ("modal-" + templateId).replace(",", "-");
-    //var templateId = "<dialog class='extension-warning with-icon'><section><h2>Warning</h2><p>The extensions on this site are experimental</p></section><section><p>The Scratch Team is <strong>not</strong> responsible for the extensions and projects on this site. Please use caution when using these extensions. <a href='#faq'>Learn More</a></p><input type='file' id='upload'/><button data-action='show' data-target='home'>Back to ScratchX home</button><button class='success'>I understand, continue</button></section></dialog>"
+    var modalId = ('modal-' + templateId).replace(',', '-');
+    //var templateId = '<dialog class='extension-warning with-icon'><section><h2>Warning</h2><p>The extensions on this site are experimental</p></section><section><p>The Scratch Team is <strong>not</strong> responsible for the extensions and projects on this site. Please use caution when using these extensions. <a href='#faq'>Learn More</a></p><input type='file' id='upload'/><button data-action='show' data-target='home'>Back to ScratchX home</button><button class='success'>I understand, continue</button></section></dialog>'
 
 //<input type='file' id='upload'/>
-    $modalwrapper = $("<div class='modal-fade-screen'><div class='modal-inner'></div></div>");
-    var $modal = CreateFromTemplate(modalId, templateId, "dialog", "body", $modalwrapper, data);
+    $modalwrapper = $('<div class='modal-fade-screen'><div class='modal-inner'></div></div>');
+    var $modal = CreateFromTemplate(modalId, templateId, 'dialog', 'body', $modalwrapper, data);
 
-    $modal.addClass("modal");
+    $modal.addClass('modal');
 
-    $(".modal-fade-screen", $modal)
-        .addClass("visible")
-        .click(function(e){if ($(e.target).is($(this))) $(this).trigger("modal:exit")});
+    $('.modal-fade-screen', $modal)
+        .addClass('visible')
+        .click(function(e){if ($(e.target).is($(this))) $(this).trigger('modal:exit')});
 
-    $(".modal-close", $modal).click(function(e){
+    $('.modal-close', $modal).click(function(e){
         e.preventDefault();
-        console.log("modal-close ");
-        $(document).trigger("modal:exit")
+        console.log('modal-close ');
+        $(document).trigger('modal:exit')
     });
     
-    $("body").addClass("modal-open");
+    $('body').addClass('modal-open');
 
-    $(document).one("modal:exit page:show editor:extensionLoaded", function(e){
-        $("body").removeClass("modal-open");
+    $(document).one('modal:exit page:show editor:extensionLoaded', function(e){
+        $('body').removeClass('modal-open');
         Scratch.FlashApp.ASobj.ASsetModalOverlay(false);
         $modal.remove();
     });
-    console.log("returned1");
+    console.log('returned1');
     return $modal;
 }
 
-      $modal = sModal("template-warning", null);
-      inputElement = document.getElementById("upfile");
-      inputElement.addEventListener("change", function(event){
+      $modal = sModal('template-warning', null);
+      inputElement = document.getElementById('upfile');
+      inputElement.addEventListener('change', function(event){
         file = inputElement.files,
         fsize = file.length;
 
     for (i=0; i < fsize; i++) {
-        console.log("Filename: " + file[i].name);
-        console.log("Type: " + file[i].type);
-        console.log("Size: " + file[i].size + " bytes");
+        console.log('Filename: ' + file[i].name);
+        console.log('Type: ' + file[i].type);
+        console.log('Size: ' + file[i].size + ' bytes');
     }
 
     var reader = new FileReader();
         reader.onload = function(event) {
             var contents = event.target.result;
-            console.log("File contents: " + contents);
+            console.log('File contents: ' + contents);
             };
         reader.readAsText(file[0]);
 
       },false);
-    $("button", $modal).click(function(e){
+    $('button', $modal).click(function(e){
        // e.preventDefault();
-        console.log("modal-button 2");
-        var evt = document.createEvent("MouseEvents");
-        evt.initEvent("click", true, false);
-        document.getElementById("upfile").dispatchEvent(evt);
+        console.log('modal-button 2');
+        var evt = document.createEvent('MouseEvents');
+        evt.initEvent('click', true, false);
+        document.getElementById('upfile').dispatchEvent(evt);
         
-        //$(document).trigger("modal:exit")
+        //$(document).trigger('modal:exit')
     });
       /*fileSelector = document.createElement('input');
       fileSelector.setAttribute('type', 'file');
       document.body.appendChild(fileSelector);
-      var evt = document.createEvent("MouseEvents");
-      evt.initEvent("click", true, false);
+      var evt = document.createEvent('MouseEvents');
+      evt.initEvent('click', true, false);
      zxzx fileSelector.dispatchEvent(evt);*/
     }
 
     ext.newShape = function() {
         if(sceneWindow!=null){
-        console.log("ScratchExtensions", ScratchExtensions);
+        console.log('ScratchExtensions', ScratchExtensions);
         sceneWindow.addShape();
         updateExtension();
-       return "loaded";
+       return 'loaded';
    }else{
-       return "not loaded";
+       return 'not loaded';
    }
     };
 
@@ -194,7 +194,7 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
     }
 
     // Block and block menu descriptions
-        descriptor.blocks.push(['', 'New 3D World %m.Scenes Width: %n Height: %n', 'initWorld', "Grass", 10, 10]);
+        descriptor.blocks.push(['', 'New 3D World %m.Scenes Width: %n Height: %n', 'initWorld', 'Grass', 10, 10]);
         descriptor.blocks.push(['r', 'New Shape', 'newShape']);
         descriptor.blocks.push(['null', 'Upload', 'uploadFile']);
     //
