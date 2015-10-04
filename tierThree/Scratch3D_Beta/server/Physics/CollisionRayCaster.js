72f0baf64dd5db63163a7a00d1c4bebbba51b7bc
goodwinj14 <goodwinj14@yahoo.com>
2015-10-04 12:52:58 -0400

    Blocks UX change

---
+++ tierThree/Scratch3D_Beta/server/Physics/CollisionRayCaster.js
@@ -0,0 +1,124 @@
+CollisionDetection = {};
+var raycasters = [];
+var raycastersIDS = [];
+//The extention source to post the message two
+CollisionDetection.messageSource = null;
+//The origin of the extention we wish to pase the object 
+//contaning the touch events to.
+CollisionDetection.origin = null;
+
+//The last object that was sent to the scratch extension window.
+CollisionDetection.lastTouching = "";
+
+CollisionDetection.appendCasterTo = function(casterObject, touchObject, casterID, touchID){
+	var casterObj = new CollisionDetection.RayCaster(casterObject, casterID, touchObject, touchID);
+	raycasters[casterID] = casterObj;
+	raycastersIDS.push(casterID);
+
+}
+
+//Add a new object to a existering Ray Caster
+//Takes in the id of the caster object and the actual object to be added to it
+CollisionDetection.addObjectToCaster = function(casterObject, addObject, addObjectID){
+	addObject.name = addObjectID;
+	if(raycasters[casterObject]!=null){
+		raycasters[casterObject].conntactObjects.push(addObject);
+	}
+}
+//The update function checks to see if there has been a change in the touch state of any of
+//the objects in it list of objects
+CollisionDetection.update = function(){
+
+	var touchReturn ="";
+
+	for (var i = raycastersIDS.length - 1; i >= 0; i--) {
+		var temp = raycasters[raycastersIDS[i]];
+		temp.CollisionCheck();
+		if(temp.isTouching.length>0){
+		touchReturn = touchReturn+temp.name+":"+temp.isTouching+";";
+		}
+	};
+
+	if(CollisionDetection.lastTouching != touchReturn){
+		CollisionDetection.lastTouching = touchReturn;
+		//If the we have had a change in the current touch state 
+		//A messgae will be passed to the extention with the new touching state data
+		if(CollisionDetection.messageSource!=null || CollisionDetection.origin!=null){
+		CollisionDetection.messageSource.postMessage("RAYCASTTOUCH_"+CollisionDetection.lastTouching,CollisionDetection.origin);
+		}
+	}
+}
+
+CollisionDetection.touchingObject = {};
+
+CollisionDetection.RayCaster = function(caster, casterID, touch, touchID){
+	//sets the name of the object to the touchID name 
+	touch.name = touchID;
+	caster.name = casterID;
+
+	//NewlyAdded
+	caster.geometry.computeBoundingSphere();
+	//
+
+	//Boolean value to check if object was touching on last update
+	this.wasTouchingLast = false;
+	this.isTouching = [];
+	//The mesh that the ray caster will be applied to
+	this.mesh = caster;
+	//The objects we want to test if the mesh is touching
+	this.conntactObjects = [];
+	this.conntactObjects.push(touch);
+	//The object ID that is associated with the mesh provided
+	this.name = casterID;
+	//The actual ray caster for this object
+	this.caster = new THREE.Raycaster();
+	this.caster.far = .21;
+	//The rays that will be projected from the origin point when we test for colisions
+	this.rays = [
+	  new THREE.Vector3(1, 0, 0),
+      new THREE.Vector3(-1, 0, 0),
+      new THREE.Vector3(0, 1, 0),
+      new THREE.Vector3(0, -1, 0),
+      new THREE.Vector3(0, 0, 1),
+      new THREE.Vector3(0, 0, -1)
+      ];
+
+      this.raysPosition = [];
+      if(caster.geometry.vertices.length>100){
+      for (var i = caster.geometry.vertices.length - 1; i-4 >= 0; i-=4) {
+      	this.raysPosition.push(caster.geometry.vertices[i]);
+      };
+  	}else{
+  		this.raysPosition = caster.geometry.vertices;
+  		
+  	}
+
+      
+	//Sets the rays to the caster
+
+	this.CollisionCheck = function(){
+		this.isTouching = [];
+		this.mesh.updateMatrixWorld();
+		
+		for (var i = this.mesh.geometry.vertices.length - 1; i >= 0; i--) {
+			var vector = this.mesh.geometry.vertices[i].clone();
+			vector.applyMatrix4( this.mesh.matrixWorld );
+			for(var g = this.rays.length - 1; g >= 0; g--){
+			this.caster.set(vector, this.rays[g]);
+			var touching = this.caster.intersectObjects(this.conntactObjects);
+
+			//console.log("Ray castre return values",touching);
+			for (var j = touching.length - 1; j >= 0; j--) {
+				//Checks to see if the touch object has already been detected by another array
+				if(this.isTouching.indexOf(touching[j].object.name)<0){
+					
+					this.isTouching.push(touching[j].object.name);
+				}
+			};
+			}
+			
+		};
+			
+	}
+
+}
